fce68a8a8fb4eeb6403d3b7393e853b057c7cc68
Abdelali El Makkioui <abdelmkey19973@gmail.com>
2022-04-12 17:06:08 +0000

    add upload functionality

---
+++ server/helper/multer.js
@@ -0,0 +1,32 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        crypto.randomBytes(16, (err, result) => {
+            const hash = result.toString('hex');
+            const extension = file.mimetype.split("/")[1];
+            const filename = hash + "." + extension;
+
+            cb(null, filename);
+        });
+    }
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const allowedMime = ['image/jpeg', 'image/png', 'image/gif'];
+
+        if (allowedMime.includes(file.mimetype) == true)
+            cb(null, true);
+        else {
+            const error = new multer.MulterError("", file);
+            error.message = "Invalid file type";
+
+            cb(error);
+        }
+    }
+});
+
+module.exports = { upload };
