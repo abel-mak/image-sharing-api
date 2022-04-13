d7bccb78590ab09958c428d3ce34ea8aa4986493
Abdelali El Makkioui <abdelmkey19973@gmail.com>
2022-04-13 11:54:49 +0000

    modify configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,25 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": "password",
+    "password": process.env.DB_PASSWORD,
     "database": "database_development",
-    "host": "db",
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "database_test",
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "database_production",
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
